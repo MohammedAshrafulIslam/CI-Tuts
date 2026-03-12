b848441615869a50a1b13b9a5c6c5fdcf24a8cf8
MohammedAshrafulIslam <ashraf22@my.yorku.ca>
2026-03-12 16:55:16 -0400

    formatting fix

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('  hello world');
- }
-);
+  res.send('hello world');
+});
 
 module.exports = app;
