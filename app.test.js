8d30ea24e4963dfcba4c8cfc247981ca4edb1421
MohammedAshrafulIslam <ashraf22@my.yorku.ca>
2026-03-12 15:34:32 -0400

    Initial commit

---
+++ app.test.js
@@ -0,0 +1,14 @@
+const request = require('supertest');
+const { describe, it, expect } = require('@jest/globals');
+const app = require('./app');
+
+describe('GET /', () => {
+ // it block is one test case!
+  it(`should return 'Works on my machine.'`, async () => {
+    const res = await request(app)
+      .get('/')
+      .expect(200);
+
+    expect(res.text).toBe('Works on my machine.');
+  });
+});
